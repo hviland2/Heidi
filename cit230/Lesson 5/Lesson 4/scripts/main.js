0c120cebb662129bb08d8123daf6b3b96f4246dd
Heidi Viland <hviland2@byui.edu>
2019-10-18 11:54:49 -0600

    solved

--- cit230/Lesson 5/Lesson 4/scripts/main.js
+++
@@ -1,8 +0,0 @@
-  
-var currentDate = new Date();
-var currentYear = currentDate.getFullYear();
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-document.getElementById("currentYear").innerHTML = days[currentDate.getDay()] + ", " + months[currentDate.getMonth()]+ " " + currentDate.getDate() + ", " + currentYear;
-
